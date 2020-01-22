8d9382d90d68cad367e23cafe6ec9cd66ac1f856
bsaipraneet9 <43109963+bsaipraneet9@users.noreply.github.com>
2020-01-23 00:01:46 +0530

    Add files via upload

---
+++ ADXL_FINAL.ino
@@ -0,0 +1,47 @@
+ int av1=A3;
+int av2=A4;
+int a,b,x,y;
+void setup() {
+ 
+ a=analogRead(av1);        
+ b=analogRead(av2);
+
+ Serial.begin(9600);
+}
+void loop() {
+x=analogRead(av1);
+y=analogRead(av2);
+Serial.println("------");
+Serial.print("X value: "); 
+Serial.println(x-a);
+Serial.print("Y value");
+Serial.println(y-b); 
+delay(300);
+
+ //for left led
+ if(x-a>30)
+ {
+
+ Serial.println("LEFT");
+ }
+//for right led
+ if(a-x>30)
+ {
+ 
+ Serial.println("RIGHT");
+ }
+
+if(b-y>30)
+ {
+ 
+ Serial.println("FORWARD");
+ }
+
+ if(y-b>30)
+ {
+ 
+ Serial.println("BACK");
+ }
+ delay(100);
+}
+
