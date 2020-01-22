fe8ee7462074373cb58ea6d3a1f3e14df857e3dc
bsaipraneet9 <43109963+bsaipraneet9@users.noreply.github.com>
2020-01-22 23:49:08 +0530

    Add files via upload

---
+++ IR_Module.ino
@@ -0,0 +1,22 @@
+const int irs = 7;
+const int led = 8;
+int detect;
+void setup() {
+  pinMode (irs, INPUT);
+  pinMode (led, OUTPUT); 
+  Serial.begin (9600);
+}
+
+void loop() {
+  detect = digitalRead (irs);
+  Serial.println(detect);
+  if (detect == HIGH)
+  {
+    digitalWrite (led, HIGH);
+  }
+  else
+  {
+    digitalWrite (led, LOW);
+  }
+  delay (200);
+}
