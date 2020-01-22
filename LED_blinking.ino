5bdafdc15b02b45036a78e03745298f768293af7
bsaipraneet9 <43109963+bsaipraneet9@users.noreply.github.com>
2020-01-22 23:47:04 +0530

    Add files via upload

---
+++ LED_blinking.ino
@@ -0,0 +1,17 @@
+int a = 13;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(13,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(a,HIGH);
+delay(1000);
+digitalWrite(a,LOW);
+delay(1000);
+digitalWrite(a,HIGH);
+delay(1000);
+digitalWrite(a,HIGH);
+delay(1000);
+}
