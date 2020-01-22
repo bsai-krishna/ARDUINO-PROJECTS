8b20ccefb691930a860154570e3b3895dcad8985
bsaipraneet9 <43109963+bsaipraneet9@users.noreply.github.com>
2020-01-23 00:06:13 +0530

    Add files via upload

---
+++ dht11.ino
@@ -0,0 +1,36 @@
+#include<DHT.h>
+#define DHTPIN 3
+#define DHTTYPE DHT11
+#define led 7
+int maxhum=32;
+int maxtemp=38;
+DHT dht(DHTPIN,DHTTYPE);
+void setup(){
+pinMode(led,OUTPUT);
+Serial.begin(9600);
+dht.begin();
+}
+void loop(){
+delay(2000);
+float h=dht.readHumidity();
+float t=dht.readTemperature();
+if(isnan(h)||isnan(t)) //isnan=is not a number
+{
+  Serial.println("Failed to read from sensor"); //println is used when we need to print a statement
+  return;
+}
+if(h>maxhum||t>maxtemp)
+{digitalWrite(led,LOW);
+}
+else
+{
+digitalWrite(led,HIGH);
+}
+Serial.print("Humidity:");
+Serial.print(h);
+Serial.print("%\t");
+Serial.print("Temperature:");
+Serial.print(t);
+Serial.print("*C\n");
+}
+
